61f3a6272c24e89c91fea7e4b54f5fff3b925d04
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-09-13 20:33:44 +0100

    done

---
+++ src/views/HomeView.js
@@ -0,0 +1,20 @@
+const styles = {
+  container: {
+    minHeight: 'calc(100vh - 50px)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 500,
+    fontSize: 48,
+    textAlign: 'center',
+  },
+};
+
+const HomeView = () => (
+  <div style={styles.container}>
+  </div>
+);
+
+export default HomeView;
